3d322bac3ce977e38c078773488ca15e6c9577df
Edmund Ophie <edmund.ophie@traveloka.com>
2019-06-21 20:36:49 +0700

    init

---
+++ precache-manifest.47ff48c086cd413aa33155a94ac79afa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f77897d2ad4446b521cea7c43d1d30a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9091e4c24dfb554372ff",
+    "url": "/static/css/main.39e92b29.chunk.css"
+  },
+  {
+    "revision": "799f69f1277015682af2",
+    "url": "/static/js/2.9f91a5f4.chunk.js"
+  },
+  {
+    "revision": "9091e4c24dfb554372ff",
+    "url": "/static/js/main.054781de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
